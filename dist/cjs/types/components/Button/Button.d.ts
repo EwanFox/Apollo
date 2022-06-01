07a9dd55c636431c8901b7b4b5b99f66c8f5cff5
BananaSquares <ewan@ewanfox.com>
2022-06-01 19:09:11 -0400

    fix rollup, remove node_modules

--- dist/cjs/types/components/Button/Button.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-export interface ButtonProps {
-    onClick: () => void;
-    width: number;
-    name: string;
-    dark: boolean;
-}
-declare const Button: (props: ButtonProps) => JSX.Element;
-export default Button;
